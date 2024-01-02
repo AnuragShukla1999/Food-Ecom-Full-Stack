7e01466f629f950e13ceb3cef6ede1b16f4cccf0
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-01-03 02:03:46 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = async (req, res, next) => {
+    try {
+        console.log(req.headers)
+        const token = req.headers.authorization;
+
+        const decode =  jwt.verify(token.split(' ')[1],process.env.JWT_SECRET);
+        
+        console.log(decode);
+
+        req.user = decode;
+
+        // console.log(req.user)
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            error : "Invalid input..."
+        });
+    }
+}
